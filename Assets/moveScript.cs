021701a6096d56983817c400ab872953bfcbd199
yod <yod1498@yahoo.com>
2017-04-04 22:03:36 +0700

    add test4

--- Assets/moveScript.cs
+++ Assets/moveScript.cs
@@ -5,13 +5,14 @@ public class moveScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//test2
 		//test3
-		transform.position += new Vector3 (Input.GetAxis("Horizontal"),0,0);			
+		//test4
+		transform.position += new Vector3 (Input.GetAxis("Horizontal"),0,0);
 	}
 }
